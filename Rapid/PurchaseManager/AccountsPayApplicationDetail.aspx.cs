3063d5d3df1955035016054cfcb208c6e6b3653d
阳俊 <yangjun1@beisen.com>
2016-07-26 13:02:50 +0800

    O:修复由于Static导致的页面显示问题。【备注：以后最好少用页面级别的Static字段】

--- Rapid/PurchaseManager/AccountsPayApplicationDetail.aspx.cs
+++ Rapid/PurchaseManager/AccountsPayApplicationDetail.aspx.cs
@@ -1,21 +1,26 @@
-﻿using System;
+﻿using BLL;
+using DAL;
+using System;
 using System.Collections.Generic;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using BLL;
-using DAL;
 
 namespace Rapid.PurchaseManager
 {
     public partial class AccountsPayApplicationDetail : System.Web.UI.Page
     {
-        public static string show = "inline";
+        public string show = "inline";
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            Bind();
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
-
                 if (ToolManager.CheckQueryString("SQ"))
                 {
                     show = "none";
@@ -24,18 +29,18 @@ namespace Rapid.PurchaseManager
                 {
                     show = "none";
                 }
-              
+
                 //Guid, InvoiceNumber, BillingDate, Remark
                 if (Request["Guid"] != null && Request["Guid"] != "")
                 {
-                    string sql = string.Format(@" 
+                    string sql = string.Format(@"
 update
-    T_AccountsPayable_Detail 
-set 
+    T_AccountsPayable_Detail
+set
     InvoiceNumber='{0}',
     BillingDate='{1}',
     Remark='{2}'
-where 
+where
     Guid='{3}'", Request["InvoiceNumber"], Request["BillingDate"], Request["Remark"], Request["Guid"]);
                     string error = string.Empty;
                     Response.Write(SqlHelper.ExecuteSql(sql, ref error) ? "ok" : error);
@@ -55,7 +60,7 @@ where
                     List<string> sqls = new List<string>();
                     string error = string.Empty;
                     string guids = ToolManager.GetQueryString("js");
-                    string sql = string.Format(@" 
+                    string sql = string.Format(@"
 update T_AccountsPayable_Detail set PaymentAmount=SumPrice,PaymentDate='{0}',IsSettlement='是' where guid in ({1}) ", DateTime.Now.ToString("yyyy-MM-dd"), guids);
                     sqls.Add(sql);
                     sql = string.Format(@" update T_AccountsPayable_Main set ActualPaymentsAmount=(
@@ -69,7 +74,6 @@ where OrdersNumber='{0}' and CreateTime ='{1}'", ordersnumber, createtime);
                 }
                 Bind();
             }
-
         }
 
         private void Bind()
@@ -87,11 +91,6 @@ where OrdersNumber='{0}' and CreateTime ='{1}'", ordersnumber, createtime);
 select '合计','','','','','',SUM(采购数量),SUM(到货数量),'',SUM(总价),'','','','','','','','','','' from V_AccountsPayDetail {0}", condition);
             rpList.DataSource = SqlHelper.GetTable(sql);
             rpList.DataBind();
-        }
-
-        protected void btnSearch_Click(object sender, EventArgs e)
-        {
-            Bind();
         }
     }
 }
