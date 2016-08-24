e6642183da5792686997a42e1508d4f397585102
阳俊 <yangjun1@beisen.com>
2016-08-25 01:25:13 +0800

    O:解决问题

--- Rapid/FinancialManager/Report_CustomerProductSaleMonth.aspx
+++ Rapid/FinancialManager/Report_CustomerProductSaleMonth.aspx
@@ -9,7 +9,7 @@
 </head>
 <body>
     <form id="form1" runat="server">
-        <div style="text-align: center; font-size: 25px; margin: 10px; margin-top: 50px;">
+        <div style="text-align: center; font-size: 25px; margin: 10px; margin-top: 30px;">
             客户产成品销售月报表
         </div>
         <div style="width: 100%; height: 20px;"></div>
@@ -27,24 +27,91 @@
         &nbsp;&nbsp;<asp:Button runat="server" ID="btnSearch" Text="查询" OnClick="btnSearch_Click" />
         &nbsp;&nbsp;<input type="button" value="打印" />
         &nbsp;&nbsp;<asp:Button runat="server" ID="btnExp" Text="导出Excel" OnClick="btnExp_Click" />
+        &nbsp;&nbsp;<asp:Button runat="server" ID="btnBom" Text="按照BOM分解" OnClick="btnBom_Click" />
         <div style="width: 100%; height: 20px;"></div>
 
         <table cellpadding="1" cellspacing="1" class="border">
             <tr>
                 <% foreach (System.Data.DataColumn column in DtResult.Columns)
-                    { %>
+                   { %>
                 <td><%=  column.ColumnName %></td>
                 <%} %>
             </tr>
             <%foreach (System.Data.DataRow dr in DtResult.Rows)
-                {%>
+              {%>
             <tr>
                 <% foreach (System.Data.DataColumn column in DtResult.Columns)
-                    { %>
+                   { %>
                 <td><%= dr[column.ColumnName]%></td>
                 <%} %>
             </tr>
             <%} %>
+        </table>
+        <div style="width: 100%; height: 20px;"></div>
+
+        <div style="text-align: center; font-size: 25px; margin: 10px; margin-top: 50px;">
+            产成品分解为原材料销售月度报表
+        </div>
+        <table cellpadding="1" cellspacing="1" class="border">
+            <tr>
+                <td>原材料编号</td>
+                <td>1月份</td>
+                <td>2月份</td>
+                <td>3月份</td>
+                <td>4月份</td>
+                <td>5月份</td>
+                <td>6月份</td>
+                <td>7月份</td>
+                <td>8月份</td>
+                <td>9月份</td>
+                <td>10月份</td>
+                <td>11月份</td>
+                <td>12月份</td>
+                <td>数量合计</td>
+                <td>采购单价</td>
+                <td>采购单价合计</td>
+            </tr>
+            <%
+                var temp = MonthMaterils.OrderByDescending(t => t.Count).ToList();
+                foreach (var model in temp)
+                { %>
+            <tr>
+                <td><%=model.MaterilNumber   %></td>
+                <td><%=model.January   %></td>
+                <td><%=model.February  %></td>
+                <td><%=model.March     %></td>
+                <td><%=model.April     %></td>
+                <td><%=model.May       %></td>
+                <td><%=model.June      %></td>
+                <td><%=model.July      %></td>
+                <td><%=model.August    %></td>
+                <td><%=model.September %></td>
+                <td><%=model.October   %></td>
+                <td><%=model.November  %></td>
+                <td><%=model.December  %></td>
+                <td><%=model.Count  %></td>
+                <td><%=model.UnitPrice  %></td>
+                <td><%=model.CountPrice  %></td>
+            </tr>
+            <%} %>
+            <tr>
+                <td>合计</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td><%=MonthMaterils.Sum(t=>t.CountPrice) %></td>
+            </tr>
         </table>
     </form>
 </body>
--- Rapid/FinancialManager/Report_CustomerProductSaleMonth.aspx.cs
+++ Rapid/FinancialManager/Report_CustomerProductSaleMonth.aspx.cs
@@ -11,8 +11,30 @@ using System.Web.UI.WebControls;
 
 namespace Rapid.FinancialManager
 {
+    public class MonthMateril
+    {
+        public double April { get; set; }
+        public double August { get; set; }
+        public double Count { get; set; }
+        public double CountPrice { get; set; }
+        public double December { get; set; }
+        public double February { get; set; }
+        public double January { get; set; }
+        public double July { get; set; }
+        public double June { get; set; }
+        public double March { get; set; }
+        public string MaterilNumber { get; set; }
+        public double May { get; set; }
+        public double November { get; set; }
+        public double October { get; set; }
+        public double September { get; set; }
+        public double UnitPrice { get; set; }
+    }
+
     public class MonthModel
     {
+        public Dictionary<string, double> MaterialCounts = new Dictionary<string, double>();
+
         public MonthModel(string englishName, string month, string chineseName)
         {
             this.EnglishName = englishName;
@@ -27,7 +49,50 @@ namespace Rapid.FinancialManager
 
     public partial class Report_CustomerProductSaleMonth : System.Web.UI.Page
     {
+        public static DataTable DtBomInfo = new DataTable();
+        public static DataTable DtProduct = new DataTable();
         public static DataTable DtResult = new DataTable();
+        public static List<MonthMateril> MonthMaterils = new List<MonthMateril>();
+
+        protected void btnBom_Click(object sender, EventArgs e)
+        {
+            List<MonthMateril> resultList = GetRsultModel();
+
+            List<MonthModel> months = new List<MonthModel>() {
+                new MonthModel ("January","01","1月份"),
+                new MonthModel ("February","02","2月份"),
+                new MonthModel ("March","03","3月份"),
+                new MonthModel ("April","04","4月份"),
+                new MonthModel ("May","05","5月份"),
+                new MonthModel ("June","06","6月份"),
+                new MonthModel ("July","07","7月份"),
+                new MonthModel ("August","08","8月份"),
+                new MonthModel ("September","09","9月份"),
+                new MonthModel ("October","10","10月份"),
+                new MonthModel ("November","11","11月份"),
+                new MonthModel ("December","12","12月份")
+            };
+            DtResult = GetTable();
+            DtProduct = GetProduct();
+            DtBomInfo = GetBomInfo();
+            for (int i = 1; i <= 12; i++)
+            {
+                foreach (DataRow dr in DtResult.Rows)
+                {
+                    if (!dr[0].ToString().Equals("合计"))
+                    {
+                        string columnName = i.ToString() + "月份";
+                        //dr[columnName]
+                        //months[i-1].MaterialCounts
+                        var value = GetMateriNumbers(dr["产成品编号"].ToString(), dr["版本"].ToString(), Convert.ToDouble(dr[columnName].ToString()));
+                        SetValue(months[i - 1], value);
+                    }
+                }
+            }
+
+            Union(resultList, months);
+            MonthMaterils = resultList;
+        }
 
         protected void btnExp_Click(object sender, EventArgs e)
         {
@@ -49,6 +114,76 @@ namespace Rapid.FinancialManager
         private void Bind()
         {
             DtResult = GetTable();
+        }
+
+        private DataTable GetBomInfo()
+        {
+            string sql = @"
+select 包号 PackgeName,产成品编号 ProductNumber,版本 Version,原材料编号 MateriNumber,单机用量 UnitQty
+ from [V_BOM_Count]";
+            return SqlHelper.GetTable(sql);
+        }
+
+        private Dictionary<string, double> GetMateriNumbers(string productNumber, string version, double qty)
+        {
+            Dictionary<string, double> materiNumbers = new Dictionary<string, double>();
+
+            DataRow[] drs = DtProduct.Select("ProductNumber='" + productNumber + "' and Version='" + version + "'");
+            if (drs != null && drs.Count() > 0)
+            {
+                string type = drs[0]["Type"].ToString();
+                DataRow[] drTemps = null;
+                if (type.Equals("包"))
+                {
+                    drTemps = DtBomInfo.Select("PackgeName='" + productNumber + "'");
+                }
+                else
+                {
+                    drTemps = DtBomInfo.Select("ProductNumber='" + productNumber + "' and Version='" + version + "'");
+                }
+
+                if (drTemps != null && drTemps.Count() > 0)
+                {
+                    foreach (DataRow tempRow in drTemps)
+                    {
+                        if (materiNumbers.ContainsKey(tempRow["MateriNumber"].ToString()))
+                        {
+                            materiNumbers[tempRow["MateriNumber"].ToString()] +=
+                                Convert.ToDouble(tempRow["UnitQty"].ToString()) * qty;
+                        }
+                        else
+                        {
+                            materiNumbers.Add(tempRow["MateriNumber"].ToString(), Convert.ToDouble(tempRow["UnitQty"].ToString()) * qty);
+                        }
+                    }
+                }
+            }
+            return materiNumbers;
+        }
+
+        private DataTable GetProduct()
+        {
+            string sql = "select ProductNumber,Version,Type from Product";
+            return SqlHelper.GetTable(sql);
+        }
+
+        private List<MonthMateril> GetRsultModel()
+        {
+            List<MonthMateril> list = new List<MonthMateril>();
+            string sql = @"
+select m. MaterialNumber,ISNULL(t.Prcie,0) Prcie from MarerialInfoTable m left join
+(select MaterialNumber,max(Prcie)  Prcie from MaterialSupplierProperty group by MaterialNumber)
+t on t.MaterialNumber=m.MaterialNumber";
+            DataTable dt = SqlHelper.GetTable(sql);
+            foreach (DataRow dr in dt.Rows)
+            {
+                list.Add(new MonthMateril()
+                {
+                    MaterilNumber = dr["MaterialNumber"].ToString(),
+                    UnitPrice = Convert.ToDouble(dr["Prcie"].ToString())
+                });
+            }
+            return list;
         }
 
         private DataTable GetTable()
@@ -121,14 +256,14 @@ D as (
 {1}
 select
 --C.CustomerName as 客户名称,
-B.ProductNumber as 产成品编号,
 B.CustomerProductNumber as 客户产成品编号,
+B.ProductNumber as 产成品编号,
 B.Version as 版本
 {2},
 B.Qty  as 数量合计,
 D.UnitPrice as 销售未税单价,
-B.Qty*D.UnitPrice as 销售未税合计,
-D.QuoteNumber as 最新报价单
+B.Qty*D.UnitPrice as 销售未税合计--,
+--D.QuoteNumber as 最新报价单
 
 from B
 left join D on B.ProductNumber=D.ProductNumber and B.Version=D.Version
@@ -138,6 +273,18 @@ left join C on B.CustomerProductNumber=C.CustomerProductNumber and B.Version=C.V
 order by B.CustomerProductNumber,B.Version
 ", year, sbMonthSql.ToString(), sbMonthTitleSql.ToString(), sbMonthLeftJoinSql.ToString(), condition.ToString());
             DataTable dt = SqlHelper.GetTable(sql);
+            List<string> columnNames = new List<string>();
+            foreach (DataColumn item in dt.Columns)
+            {
+                columnNames.Add(item.ColumnName);
+            }
+
+            DataTable dtCopy = dt.Copy();
+
+            DataView dv = dt.DefaultView;
+            dv.Sort = "数量合计 DESC";
+            dt = dv.ToTable(true, columnNames.ToArray());
+
             object obj = dt.Compute("sum(销售未税合计)", "true");
             DataRow drSumRow = dt.NewRow();
             drSumRow[0] = "合计";
@@ -145,6 +292,96 @@ order by B.CustomerProductNumber,B.Version
             dt.Rows.Add(drSumRow);
 
             return dt;
+        }
+
+        private void SetValue(MonthModel monthModel, Dictionary<string, double> value)
+        {
+            var oldValue = monthModel.MaterialCounts;
+            foreach (var item in value)
+            {
+                if (oldValue.ContainsKey(item.Key))
+                {
+                    oldValue[item.Key] += item.Value;
+                }
+                else
+                {
+                    oldValue[item.Key] = item.Value;
+                }
+            }
+        }
+
+        private void SetValues(MonthMateril monthMateril, List<MonthModel> months)
+        {
+            //var materialCounts = months[0].MaterialCounts;
+            if (months[0].MaterialCounts.ContainsKey(monthMateril.MaterilNumber))
+            {
+                monthMateril.January = months[0].MaterialCounts[monthMateril.MaterilNumber];
+            }
+            if (months[1].MaterialCounts.ContainsKey(monthMateril.MaterilNumber))
+            {
+                monthMateril.February = months[1].MaterialCounts[monthMateril.MaterilNumber];
+            }
+            if (months[2].MaterialCounts.ContainsKey(monthMateril.MaterilNumber))
+            {
+                monthMateril.March = months[2].MaterialCounts[monthMateril.MaterilNumber];
+            }
+            if (months[3].MaterialCounts.ContainsKey(monthMateril.MaterilNumber))
+            {
+                monthMateril.April = months[3].MaterialCounts[monthMateril.MaterilNumber];
+            }
+            if (months[4].MaterialCounts.ContainsKey(monthMateril.MaterilNumber))
+            {
+                monthMateril.May = months[4].MaterialCounts[monthMateril.MaterilNumber];
+            }
+            if (months[5].MaterialCounts.ContainsKey(monthMateril.MaterilNumber))
+            {
+                monthMateril.June = months[5].MaterialCounts[monthMateril.MaterilNumber];
+            }
+            if (months[6].MaterialCounts.ContainsKey(monthMateril.MaterilNumber))
+            {
+                monthMateril.July = months[6].MaterialCounts[monthMateril.MaterilNumber];
+            }
+            if (months[7].MaterialCounts.ContainsKey(monthMateril.MaterilNumber))
+            {
+                monthMateril.August = months[7].MaterialCounts[monthMateril.MaterilNumber];
+            }
+            if (months[8].MaterialCounts.ContainsKey(monthMateril.MaterilNumber))
+            {
+                monthMateril.September = months[8].MaterialCounts[monthMateril.MaterilNumber];
+            }
+            if (months[9].MaterialCounts.ContainsKey(monthMateril.MaterilNumber))
+            {
+                monthMateril.October = months[9].MaterialCounts[monthMateril.MaterilNumber];
+            }
+            if (months[10].MaterialCounts.ContainsKey(monthMateril.MaterilNumber))
+            {
+                monthMateril.November = months[10].MaterialCounts[monthMateril.MaterilNumber];
+            }
+            if (months[11].MaterialCounts.ContainsKey(monthMateril.MaterilNumber))
+            {
+                monthMateril.December = months[11].MaterialCounts[monthMateril.MaterilNumber];
+            }
+        }
+
+        private void Union(List<MonthMateril> resultList, List<MonthModel> months)
+        {
+            foreach (var model in resultList)
+            {
+                SetValues(model, months);
+                model.Count = model.January
+                    + model.February
+                    + model.March
+                    + model.April
+                    + model.May
+                    + model.June
+                    + model.July
+                    + model.August
+                    + model.September
+                    + model.October
+                    + model.November
+                    + model.December;
+                model.CountPrice = model.UnitPrice * model.Count;
+            }
         }
     }
 }
